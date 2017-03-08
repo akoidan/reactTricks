d9b6af50460f4ec89711011aeb941d4ca7bcedce
Andrew Koidan <deathangel908@gmail.com>
2017-03-09 00:23:19 +0200

    Flux dispatcher https://www.youtube.com/watch?v=MZfCVq5iCBw&list=PLoYCgNOIyGABj2GQSlDRjgvXtqfDxKm5b&index=11

---
+++ src/dispatcher.js
@@ -0,0 +1,8 @@
+/**
+ * Created by andrew on 3/9/17.
+ */
+import {Dispatcher} from "flux";
+
+let dispatcher2 = new Dispatcher;
+window.dispatcher = dispatcher2;
+export default dispatcher2;
--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -1,7 +1,9 @@
 /**
  * Created by andrew on 3/8/17.
  */
-import {EventEmitter} from "events"
+import {EventEmitter} from "events";
+import dispatcher from "../dispatcher";
+
 
 class TodoStore extends EventEmitter {
   constructor() {
@@ -28,10 +30,20 @@ class TodoStore extends EventEmitter {
   getAll() {
     return this.todos;
   }
+
+  handleActions(action) {
+    switch (action.type) {
+      case "CREATE_TODO": {
+        this.createTodo(action.text);
+      }
+    }
+  }
 }
 
 const todoStore = new TodoStore('default');
-window.todoStore = todoStore;
+window.todoStore = todoStore; //TODO
 // this allows to print in browser console
 //todoStore.createTodo('sd');
+// dispatcher.dispatch({type: "CREATE_TODO", text: "NEW TODO"});
+dispatcher.register(todoStore.handleActions.bind(todoStore));
 export default todoStore;
