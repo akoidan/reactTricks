f01a3ea9545434c3ccd0cc362811bcbc2cfa3a35
Andrew Koidan <deathangel908@gmail.com>
2017-03-13 17:45:15 +0200

    multiple reducers https://www.youtube.com/watch?v=gBER4Or86hE&index=18&list=PLoYCgNOIyGABj2GQSlDRjgvXtqfDxKm5b

--- src/js/client.js
+++ src/js/client.js
@@ -1,23 +1,31 @@
 import {combineReducers, createStore} from "redux"
 
-
-
-const reducer = function (state, action) {
-  if (action.type === "INC") {
-    return state + action.payload;
-  } else if (action.type === "DEC") {
-    return state - action.payload;
-  } else {
-    return state;
+const userReducer = function (state = {}, action) {
+  switch (action.type) {
+    case "CHANGE_NAME":
+      state = {...state, name: action.payload};
+      break;
+    case "CHANGE_AGE":
+      state = {...state, age: action.payload};
+      break;
   }
+  return state;
 };
 
-const store = createStore(reducer, 0);
+const tweetReducer = function (state = [], action) {
+  return state;
+};
+
+
+const reducer = combineReducers(
+  {user: userReducer, tweet: tweetReducer}
+  );
+
+const store = createStore(reducer);
 store.subscribe(() => {
   console.log("store changed", store.getState());
 });
 
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "INC", payload: 3});
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "DEC", payload: 1});
+store.dispatch({type: "CHANGE_NAME", payload: "will"});
+store.dispatch({type: "CHANGE_AGE", payload: 35});
+store.dispatch({type: "CHANGE_AGE", payload: 36});
