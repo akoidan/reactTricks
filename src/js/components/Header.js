9f3876461ba2ba8b9c990b8ca7381023a5c0fc3b
Andrew Koidan <deathangel908@gmail.com>
2017-03-21 15:30:49 +0200

    hotfix

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,6 +1,24 @@
 import React from "react";
-
+import ReactPaginate from 'react-paginate';
 import Title from "./Header/Title";
+
+export class CommentList extends React.Component {
+  render() {
+    let commentNodes = this.props.data.map(function(comment, index) {
+      return (
+        <div key={index}>{comment.comment}</div>
+      );
+    });
+
+    return (
+      <div id="project-comments" className="commentList">
+        <ul>
+          {commentNodes}
+        </ul>
+      </div>
+    );
+  }
+};
 
 export default class Header extends React.Component {
   handleChange(e) {
@@ -8,12 +26,28 @@ export default class Header extends React.Component {
     this.props.changeTitle(title);
   }
 
+
+  onPageChange(event) {
+    console.log(event);
+  }
+
   render() {
     return (
-      <div>
+      <form>
         <Title title={this.props.title} />
         <input value={this.props.title} onChange={this.handleChange.bind(this)} />
-      </div>
+        <ReactPaginate previousLabel={"previous"}
+                       nextLabel={"next"}
+                       breakLabel={<a href="">...</a>}
+                       breakClassName={"break-me"}
+                       pageCount={10}
+                       marginPagesDisplayed={2}
+                       pageRangeDisplayed={5}
+                       onPageChange={this.onPageChange}
+                       containerClassName={"pagination"}
+                       subContainerClassName={"pages pagination"}
+                       activeClassName={"active"} />
+      </form>
     );
   }
 }
